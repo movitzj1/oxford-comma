75cdb64eb367e83d6bbba539752cc91797bbb8d5
Justin Movitz <movitzj1@montclair.edu>
2021-01-19 15:37:12 -0500

    commit tree b3ef0adf6b7b2288da046168d2f1ce400bcc7479

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
+  return array[0] if array.length <= 1
+  return array.join(' and ') if array.length == 2
 
+  last = array.pop
+  # array.join(', ')  + ', and' + last
+  "#{array.join(', ')}, and #{last}"
 end
+
